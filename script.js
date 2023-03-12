3157be6b611e5da1df74355eca3464a12a41438a
Leandro Gabriel Faria <leandrogabrielfaria05@gmail.com>
2023-03-12 00:45:42 -0300

    adicionando o conversor

--- script.js
+++ script.js
@@ -4,6 +4,7 @@ form.addEventListener('submit', handleSubmit)
 const inputValue = document.getElementById('value-real');
 const selectedCurrency = document.getElementById('currency');
 const result = document.getElementById('result');
+let valueConverted = 0;
 
 function handleSubmit(e) {
     e.preventDefault();
@@ -13,6 +14,27 @@ function handleSubmit(e) {
         return;
     } else if(!selectedCurrency.value) {
         alert('Escolha uma moeda!')
-
+        return;
     }
+
+    converter()
 }
+
+function converter() {
+    if(selectedCurrency.value === 'eur') {
+    valueConverted = inputValue.value * 5.55;
+    result.innerHTML = valueformatter('pt-BR', 'EUR' )
+
+    } else if(selectedCurrency.value === 'dol') {
+        valueConverted = inputValue.value * 5.22
+        result.innerHTML = valueformatter('pt-BR', 'USD' )
+    }
+
+    inputValue.value = ' ';
+    selectedCurrency.value = ' ';
+};
+
+function valueformatter(locale, currency) {
+    const value = valueConverted.toLocaleString(`${locale}` , {style: 'currency' , currency: `${currency}`});
+    return `${value}`
+};
